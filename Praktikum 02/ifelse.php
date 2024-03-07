e6b334628e4d014971bb59e7d36ad85986aaf483
faturahmanalfarisi <faturahmanalfarisi1@gmail.com>
2024-03-07 18:08:04 +0700

    Tugas Praktikum 02

---
+++ Praktikum 02/ifelse.php
@@ -0,0 +1,19 @@
+<?php
+
+$nilai = 90;
+
+$keterangan = "";
+
+if ($nilai > 90){
+    $keterangan = "sangat baik";
+}
+
+elseif ($nilai > 70 || $nilai <= 90){
+    $keterangan = "lumayan baik";
+}
+
+else {
+    $keterangan = "sip dah";
+}
+
+echo $keterangan;
---
+++ Praktikum 02/submit.php
@@ -0,0 +1,6 @@
+<?php
+
+$username =$_POST['username'];
+$username =$_POST['password'];
+
+echo "<h1>Selamat Datang $username</h1>";
